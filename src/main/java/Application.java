3dedba89c950c2a6fea68550c7416b0df1ba6367
dilshodbek <dilshodbekotajonov95@gmail.com>
2022-06-16 17:26:43 +0500

    Init

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+/**
+ * @author "Otajonov Dilshodbek
+ * @since 6/16/22 5:16 PM (Thursday)
+ * VectorGroupProject/IntelliJ IDEA
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+
+    }
+}
