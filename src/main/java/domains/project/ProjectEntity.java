7b16f4716a4559e5efc2e3b2c3450f72e055d47a
dilshodbek <dilshodbekotajonov95@gmail.com>
2022-06-17 15:04:09 +0500

    Just commit

--- src/main/java/domains/project/ProjectEntity.java
+++ src/main/java/domains/project/ProjectEntity.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author "Otajonov Dilshodbek
@@ -22,13 +23,16 @@ public class ProjectEntity extends Auditable {
     private String docPath;
     private ProjectStatus status;
 
+    private List<ProjectColumnEntity> projectColumns;
+
     @Builder(builderMethodName = "childBuilder")
-    public ProjectEntity(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, Long createdBy, Long updatedBy, int deleted, String title, String description, String docPath, ProjectStatus status) {
+    public ProjectEntity(Long id, LocalDateTime createdAt, LocalDateTime updatedAt, Long createdBy, Long updatedBy, int deleted, String title, String description, String docPath, ProjectStatus status, List<ProjectColumnEntity> projectColumns) {
         super(id, createdAt, updatedAt, createdBy, updatedBy, deleted);
         this.title = title;
         this.description = description;
         this.docPath = docPath;
         this.status = status;
+        this.projectColumns = projectColumns;
     }
 
 
